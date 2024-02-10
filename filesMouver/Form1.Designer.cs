359db60fc6cb5a82709f16684fbbdd7cc2d55c00
serzhSur <t1807999@yandex.ru>
2024-02-11 02:57:43 +0300

    Добавьте файлы проекта.

---
+++ filesMouver/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace filesMouver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            textBox2_dirIN = new TextBox();
+            textBox3_dirOut = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            progressBar1 = new ProgressBar();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.AcceptsReturn = true;
+            textBox1.AcceptsTab = true;
+            textBox1.AllowDrop = true;
+            textBox1.Location = new Point(718, 295);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.RightToLeft = RightToLeft.No;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(622, 184);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "textBox1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1009, 485);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Show";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_ViewDestinationDir_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(718, 57);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(622, 184);
+            listBox1.TabIndex = 2;
+            // 
+            // textBox2_dirIN
+            // 
+            textBox2_dirIN.Location = new Point(11, 57);
+            textBox2_dirIN.Name = "textBox2_dirIN";
+            textBox2_dirIN.Size = new Size(622, 27);
+            textBox2_dirIN.TabIndex = 3;
+            textBox2_dirIN.Text = "textBox2 dirIN C:\\test";
+            textBox2_dirIN.TextChanged += textBox2_dirIN_TextChanged;
+            // 
+            // textBox3_dirOut
+            // 
+            textBox3_dirOut.Location = new Point(11, 149);
+            textBox3_dirOut.Name = "textBox3_dirOut";
+            textBox3_dirOut.Size = new Size(622, 27);
+            textBox3_dirOut.TabIndex = 5;
+            textBox3_dirOut.Text = "textBox3 dirOut E:\\1";
+            textBox3_dirOut.TextChanged += textBox3_dirOut_TextChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(11, 232);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 6;
+            button2.Text = "Copy files";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_CopyFiles_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(112, 232);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 7;
+            button3.Text = "Copy All";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_copyAllDirectory_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 20);
+            label1.TabIndex = 8;
+            label1.Text = "Source files path\r\n";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(718, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(153, 20);
+            label2.TabIndex = 9;
+            label2.Text = "View Source directory";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 127);
+            label3.Name = "label3";
+            label3.Size = new Size(150, 20);
+            label3.TabIndex = 10;
+            label3.Text = "Dastination files path";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(718, 272);
+            label4.Name = "label4";
+            label4.Size = new Size(184, 20);
+            label4.TabIndex = 11;
+            label4.Text = "View Dastination directory";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(11, 193);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(619, 24);
+            progressBar1.Step = 1;
+            progressBar1.TabIndex = 12;
+            progressBar1.Value = 50;
+            progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(1352, 784);
+            Controls.Add(progressBar1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox3_dirOut);
+            Controls.Add(textBox2_dirIN);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Files Mouver";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private TextBox textBox2_dirIN;
+        private TextBox textBox3_dirOut;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ProgressBar progressBar1;
+    }
+}
---
+++ filesMouver/Form1.cs
@@ -0,0 +1,184 @@
+using filesMove;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Windows.Forms;
+
+
+namespace filesMouver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_ViewDestinationDir_Click(object sender, EventArgs e) //кнопка Show обзор директроии pathOut (путь в textBox3)
+        {
+
+            string dir = textBox3_dirOut.Text;
+            if (Directory.Exists(dir))
+            {
+                string[] dirView = Directory.GetFileSystemEntries(dir);
+                foreach (string f in dirView)
+                {
+                    textBox1.Text+="\r\n"+f;
+                }
+                
+            }
+
+        }
+
+        private void textBox2_dirIN_TextChanged(object sender, EventArgs e) //при изменении текста в textBox2_dirIN
+
+        {
+            ViewDirIn();
+
+            void ViewDirIn() //обзор в listBox1 дириктории Источника(textBox2_dirIN)
+            {
+                string dirIN = textBox2_dirIN.Text;
+                if (Directory.Exists(dirIN))
+                {
+                    listBox1.Items.Clear();
+                    string[] dirView = Directory.GetDirectories(dirIN);
+                    listBox1.Items.Insert(0, "Directories:");
+                    foreach (string d in dirView)
+                    {
+                        listBox1.Items.Add(d);
+                    }
+
+                    string[] filesView = Directory.GetFiles(dirIN);
+                    listBox1.Items.Add("Files:");
+                    foreach (string f in filesView)
+                    {
+                        listBox1.Items.Add(f);
+                    }
+                }
+                else
+                {
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add("No such directory");
+                }
+            }
+
+        }
+
+        private void textBox3_dirOut_TextChanged(object sender, EventArgs e) //событие на изменение в textBox3_dirOut
+        {
+            ViewDirOut();
+
+            void ViewDirOut() // обзор директории в текстовом поле textBox1, путь из textBox3_dirOut
+            {
+                string path = textBox3_dirOut.Text;
+                if (Directory.Exists(path))
+                {
+                    textBox1.Text = "Such directory exist";
+
+                    string[] dirView = Directory.GetFileSystemEntries(path);
+                    foreach (string s in dirView)
+                    {
+                        textBox1.Text += "\r\n" + s; 
+                    }
+                }
+                else { textBox1.Text = "No such directory, new path"; }
+            }
+        }
+
+
+        private void button2_CopyFiles_Click(object sender, EventArgs e) //копирование файлов по нажатию кнопки Copy
+        {
+
+            string dirIN = textBox2_dirIN.Text;
+            string dirOut = textBox3_dirOut.Text;
+
+            if (!Directory.Exists(dirOut))
+            {
+                Directory.CreateDirectory(dirOut);
+            }
+            else 
+            { 
+                MessageBox.Show("Директория уже существует. Файлы будут перезаписаны"); 
+                //кнопка отмены нужна
+            }
+
+            if (Directory.Exists(dirIN))
+            {
+                progressBar1.Visible = true;
+                progressBar1.Minimum = 1;
+                progressBar1.Maximum = Directory.GetFiles(dirIN).Length; //максимальное значение = количеству файлов в источнике
+                progressBar1.Value = 1;
+                progressBar1.Step = 1;
+
+                string[] spisokFiles = Directory.GetFiles(dirIN);// получили массив  пути файлов в dirIN(источнике)
+
+                foreach (string s in spisokFiles) 
+                {
+                    string fileName = Path.GetFileName(s); // получаем имя этого файла
+                    
+                    string destPath = ($@"{dirOut}\{fileName}");// получаем путь назначения: путь(destination)+имя файла
+                    
+                    File.Copy(s, destPath, true); // копируем файл 
+                    
+                    progressBar1.PerformStep();
+
+                }
+
+            }
+            else { MessageBox.Show("Источник не найден"); }
+
+            textBox1.Text = "Файлы скопированны";
+
+
+        }
+
+        private void button3_copyAllDirectory_Click(object sender, EventArgs e) //перемещение каталога по нажатию кнопки Move
+        {
+
+            string dirIN = textBox2_dirIN.Text;
+            string dirOut = textBox3_dirOut.Text;
+
+            CopyDirectory(dirIN, dirOut, true); // рекурсивный метод который копирует паку с вложениями
+
+            static void CopyDirectory(string dirIN, string dirOut, bool recursive)
+            {
+                // Get information about the source directory
+                var dir = new DirectoryInfo(dirIN);
+
+                // Check if the source directory exists
+                if (!dir.Exists)
+                    throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+                // Cache directories before we start copying
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                // Create the destination directory
+                Directory.CreateDirectory(dirOut);
+
+                // Get the files in the source directory and copy to the destination directory
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    string targetFilePath = Path.Combine(dirOut, file.Name);
+                    file.CopyTo(targetFilePath);
+                }
+
+                // If recursive and copying subdirectories, recursively call this method
+                if (recursive)
+                {
+                    foreach (DirectoryInfo subDir in dirs)
+                    {
+                        string newDestinationDir = Path.Combine(dirOut, subDir.Name);
+                        CopyDirectory(subDir.FullName, newDestinationDir, true);
+                    }
+                }
+            }
+            textBox1.Text = "Католог со всеми вложениями скопирован.";
+
+        }
+
+
+    }
+}
